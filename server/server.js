50a6e52e1da870122e159d8b2665df5f441e4dc6
CJ Douglas <cj@lola.com>
2024-02-22 15:33:19 -0500

    last chance before starting from scratch

--- server/server.js
+++ server/server.js
@@ -53,7 +53,7 @@ async function run() {
 
 app.get("/", function (req, res) {
   res.sendFile(
-    path.join(__dirname, "../build/index.html"),
+    path.join(__dirname, "build","index.html"),
     function (err) {
       if (err) {
         res.status(500).send(err);
